e923365f911a108a40938af4487ae157ff8ea1ce
Rahul <Rahul@Rahul-PC>
2018-12-22 10:46:19 +0530

    Commit and push to Git

---
+++ src/main/java/com/scp/string/BootSample/BootSampleApplication.java
@@ -0,0 +1,23 @@
+/*package com.scp.string.BootSample;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSampleApplication.class, args);
+		
+	}
+	
+	
+
+	
+}
+*/
